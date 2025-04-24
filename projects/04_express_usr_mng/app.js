de7c2868aaa590b7b636b2196f1f0be91a4a1001
Dailius <dailius.rascius@gmail.com>
2025-04-24 11:54:55 +0300

    Update project API express_usr_mng

--- projects/04_express_usr_mng/app.js
+++ projects/04_express_usr_mng/app.js
@@ -19,6 +19,10 @@ app.post("/user", (req, res) => {
         });
     };
 
+    if (password.trim().length < 5) {
+        return res.status(400).json({ message: "Invalid password." });
+    };
+
     if (users[username]) {
         return res.status(409).json({
             message: "Username allready exist"
@@ -74,21 +78,65 @@ app.get("/user/:name", (req, res) => {
     })
 });
 
-app.post("/login", (req, res)=>{
+app.post("/login", (req, res) => {
     const { username, password } = req.body;
 
-    if(!username || !password){
-        return res.status(400).json({message: "Username and password are required."});
+    if (!username || !password) {
+        return res.status(400).json({ message: "Username and password are required." });
     }
 
-    if(!users[username] || users[username].password !== password){
-        return res.status(401).json({message: "Invalid user name or password"});
+    if (!users[username] || users[username].password !== password) {
+        return res.status(401).json({ message: "Invalid user name or password" });
     }
 
     res.status(200).json({
         message: "Login successful",
         userId: users[username].id
     });
+});
+
+app.put("/user/:name", (req, res) => {
+    const { name } = req.params;
+    const { email, password } = req.body;
+
+    if (!users[name]) {
+        return res.status(404).json({ message: "User not found" });
+    }
+
+    if (
+        // email === undefined 
+        !email || typeof email !== 'string' || !email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)
+    ) {
+        return res.status(400).json({ message: "Invalid email." });
+    };
+
+    if (!password || password.trim().length < 5) {
+        return res.status(400).json({ message: "Invalid password." });
+    };
+
+    users[name].email = email;
+    users[name].password = password;
+
+    res.status(200).json({
+        message: "User updated successfully",
+        user: {
+            id: users[name].id,
+            username: name,
+            email: users[name].email
+        }
+    });
+});
+
+app.delete('/user/:name', (req, res) => {
+    const { name } = req.params;
+
+    if(!users[name]){
+        return res.status(404).json({message: "User not found"});
+    }
+
+    delete users[name];
+
+    res.status(204).json({message: "removed"});
 });
 
 app.listen(port, () => {
